b2d86ad68e8ebb19b189201af99c7c77c65cbc56
Noman-khalid98 <khalidnoman093@gmail.com>
2025-01-04 18:18:25 +0500

    assignment done

---
+++ script.ts
@@ -0,0 +1,20 @@
+let tempratures:number[]  = [15,23,18,27,30,25,19,22,28,16,20,24,21,29,17]
+let newTempratures:number[] = [
+    (tempratures[0]*9/5)+32,
+    (tempratures[1]*9/5)+32,
+    (tempratures[2]*9/5)+32,
+    (tempratures[3]*9/5)+32,
+    (tempratures[4]*9/5)+32,
+    (tempratures[5]*9/5)+32,
+    (tempratures[6]*9/5)+32,
+    (tempratures[7]*9/5)+32,
+    (tempratures[8]*9/5)+32,
+    (tempratures[9]*9/5)+32,
+    (tempratures[10]*9/5)+32,
+    (tempratures[11]*9/5)+32,
+    (tempratures[12]*9/5)+32,
+    (tempratures[13]*9/5)+32,
+    (tempratures[14]*9/5)+32,
+
+]
+console.log(newTempratures)
